77dbedad8069b145ec65238c45fec7a54e6ff7d5
Donda <34602885+dondasasmita@users.noreply.github.com>
2022-10-08 10:41:04 +0800

    Formatting files

--- Logger.js
+++ Logger.js
@@ -1,17 +1,15 @@
 class Logger {
-
-    run(req, res) {
-        console.log(`**** Logger Start ${Date()} *****\n`);
-        const queryParams = req.query;
-        const userAgent = req.headers['user-agent'];
-        console.log(`User-Agent: ${userAgent} \n`);
-        for (const variable in queryParams) {
-            console.log(`${variable} : ${queryParams[variable]}`);
-        }
-        console.log('\n**** Logger End ****\n');
-        res.sendStatus(200);
-    }
-
+	run(req, res) {
+		console.log(`**** Logger Start ${Date()} *****\n`);
+		const queryParams = req.query;
+		const userAgent = req.headers['user-agent'];
+		console.log(`User-Agent: ${userAgent} \n`);
+		for (const variable in queryParams) {
+			console.log(`${variable} : ${queryParams[variable]}`);
+		}
+		console.log('\n**** Logger End ****\n');
+		res.sendStatus(200);
+	}
 }
 
 module.exports = Logger;
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 const Logger = require('./Logger');
 const logger = new Logger();
 
 app.get('/', logger.run);
 
 app.listen(port, () => {
-  console.log(`App is listening at http://localhost:${port}\n`)
-})
+	console.log(`App is listening at http://localhost:${port}\n`);
+});
